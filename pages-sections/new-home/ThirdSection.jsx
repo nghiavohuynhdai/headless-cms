25b13b7fe74f8f5e4c39c8d278e6381df848e1b9
DaiNghia0212 <dainghia12311@gmail.com>
2021-11-10 13:03:27 +0700

    change content section 3

--- pages-sections/new-home/ThirdSection.jsx
+++ pages-sections/new-home/ThirdSection.jsx
@@ -6,13 +6,13 @@ import feature2 from "../../assets/img/new-home/Access-yourself-with.png";
 import feature3 from "../../assets/img/new-home/Self-study-with-AI.png";
 import teamsStyle from "assets/jss/nextjs-material-kit-pro/pages/sectionsSections/teamsStyle";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   ...teamsStyle,
   root: {
     marginTop: 120,
     [theme.breakpoints.down("sm")]: {
       marginTop: 60,
-      padding: "0px 30px"
+      padding: "0px 30px",
     },
   },
   sectionHeader: {
@@ -22,7 +22,7 @@ const useStyles = makeStyles(theme => ({
     textAlign: "center",
     alignItems: "center",
     [theme.breakpoints.down("sm")]: {
-      marginBottom: 40
+      marginBottom: 40,
     },
     "& h2": {
       textTransform: "uppercase",
@@ -39,7 +39,7 @@ const useStyles = makeStyles(theme => ({
       marginTop: 5,
       lineHeight: 1.67,
       [theme.breakpoints.down("sm")]: {
-        fontSize: 20
+        fontSize: 20,
       },
     },
   },
@@ -59,45 +59,75 @@ const useStyles = makeStyles(theme => ({
       marginBottom: "16px",
     },
     [theme.breakpoints.down("sm")]: {
-      padding: 0
+      padding: 0,
     },
   },
 }));
 
-const ThirdSection = () => {
+const ThirdSection = (props) => {
   const classes = useStyles();
+  const data = props.data.innerBlocks;
+  let gridList = [];
+  const imageList = [feature1, feature2, feature3];
+  for (let block in data) {
+    if (data[block].name == "core/columns") {
+      gridList = data[block].innerBlocks.map((element, index) => {
+        return (
+          <Grid item xs={12} md={4} sm={6} className={classes.gridItem}>
+            <img src={imageList[index]} alt="feature-1" />
+            <div
+              dangerouslySetInnerHTML={{
+                __html: element.saveContent,
+              }}
+            ></div>
+          </Grid>
+        );
+      });
+    }
+  }
+  
   return (
     <div className={`${classes.root} ${classes.container}`}>
-      <div className={classes.sectionHeader}>
-        <h2>wHAT'S IN IT FOR YOU?</h2>
+      {/* <div className={classes.sectionHeader}>
+        <h2>WHAT'S IN IT FOR YOU?</h2>
         <h3>This PTE tool will help you</h3>
-      </div>
+      </div> */}
+      <div
+        className={classes.sectionHeader}
+        dangerouslySetInnerHTML={{
+          __html: data[0].saveContent,
+        }}
+      ></div>
       <Grid container>
-        <Grid item xs={12} md={4} sm={6} className={classes.gridItem}>
+        {/* <Grid item xs={12} md={4} sm={6} className={classes.gridItem}>
           <img src={feature1} alt="feature-1" />
           <h4>Practice with repeated PTE questions</h4>
           <p>
-            PTE question banks are updated regularly and are collected by the PTE aspirants community
-            from the real exams. The questions are also sorted according to their frequency repetition rate.
+            PTE question banks are updated regularly and are collected by the
+            PTE aspirants community from the real exams. The questions are also
+            sorted according to their frequency repetition rate.
           </p>
         </Grid>
         <Grid item xs={12} md={4} sm={6} className={classes.gridItem}>
           <img src={feature2} alt="feature-2" />
           <h4>Access yourself with Practice Tests</h4>
           <p>
-            Magic PTE Mock Tests are designed to simulate te actual PTE Exam testing experience 
-            to help users familiarize themselves with the test patterns and identify weaknesses using score reports.
+            Magic PTE Mock Tests are designed to simulate te actual PTE Exam
+            testing experience to help users familiarize themselves with the
+            test patterns and identify weaknesses using score reports.
           </p>
         </Grid>
         <Grid item xs={12} md={4} sm={12} className={classes.gridItem}>
           <img src={feature3} alt="feature-3" />
           <h4>Self-study with AI scoring</h4>
           <p>
-            This Practice Test platform has been designed with AI (Artificial Intelligence) feature.
-            AI scoring means that test takers will be marked by the automated system which mimics the real
-            scoring algorithm.
+            This Practice Test platform has been designed with AI (Artificial
+            Intelligence) feature. AI scoring means that test takers will be
+            marked by the automated system which mimics the real scoring
+            algorithm.
           </p>
-        </Grid>
+        </Grid> */}
+        {gridList}
       </Grid>
     </div>
   );
