274cb91bda7db098349e8ed0f0c91d8449a99ecd
DaiNghia0212 <dainghia12311@gmail.com>
2021-11-16 14:50:37 +0700

    update dât

--- pages-sections/new-home/SixthSection.jsx
+++ pages-sections/new-home/SixthSection.jsx
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
   sectionHeader: {
     display: "flex",
     flexDirection: "column",
-    textAlign: "center",
+    alignItem: "center",
     justifyContent: "center",
     "& img": {
       maxWidth: 400,
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,124 @@
-import React, { Component } from "react";
-import Router from "next/router";
-import Home from "./home";
-import { getHomePageAPI } from "../lib/api";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import teamsStyle from "assets/jss/nextjs-material-kit-pro/pages/sectionsSections/teamsStyle";
+import { Grid } from "@material-ui/core";
+import logoclient from "../../assets/img/new-home/logoclients.png";
+import Link from "next/link";
+import handleData from "../../lib/handleData";
 
-export default function Index({ posts }) {
-  // componentDidMount = () => {
-  //   Router.push("/home");
-  // };
-
-  let i = 0;
-  while (posts[i].title != "Home Page") {
-    i++;
-  }
-  const blocksJson = JSON.parse(posts[i].blocksJSON);
-
-  return <Home data={blocksJson} />;
-}
-
-export async function getStaticProps() {
-  const json = await getHomePageAPI();
-
-  return {
-    props: {
-      posts: json.data.posts.nodes,
+const useStyles = makeStyles(theme => ({
+  ...teamsStyle,
+  root: {
+    padding: "120px 0px",
+    backgroundColor: "rgb(246, 251, 249)",
+    backgroundImage:
+      "url(https://react-next-landing.redq.io/_next/static/images/map-18b073154fe6a34a0819d1c79f404288.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "25% center",
+    [theme.breakpoints.down("sm")]: {
+      padding: "60px 0px"
     },
-    // revalidate: 10,
-  };
-}
+  },
+  sectionHeader: {
+    textAlign: "center",
+    "& img": {
+      maxWidth: 400,
+      width: "100%",
+    },
+    "& h2": {
+      textTransform: "uppercase",
+      fontSize: 14,
+      lineHeight: 1.2,
+      fontWeight: 700,
+      color: "rgb(255, 208, 0)",
+      margin: 0,
+    },
+    "& h3": {
+      fontSize: 24,
+      fontWeight: 500,
+      color: "rgb(15, 33, 55)",
+      marginTop: 5,
+      lineHeight: 1.67,
+    },
+  },
+  signUpButton: {
+    backgroundColor: "#fadb06",
+    border: "1px solid #fadb06",
+    color: "#000000",
+    height: "100%",
+    minWidth: "150px",
+    padding: "8px 22px",
+    height: "46px",
+    "&:hover": {
+      backgroundColor: "#fadb06",
+      border: "1px solid #fadb06",
+      color: "#000000",
+    },
+  },
+  gridItem: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  imageLogos: {
+    width: "50%",
+    padding: "0 15px",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%"
+    },
+  },
+  sectionContent: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  innerContent: {
+    padding: "0 15px",
+    "& h3": {
+      fontSize: "48px",
+      fontWeight: 400,
+      [theme.breakpoints.down("sm")]: {
+        fontSize: "26px"
+      },
+    },
+    "& p": {
+      fontSize: "16px",
+      fontWeight: 400,
+      marginBottom: 33,
+    },
+  },
+}));
+
+const SeventhSection = (props) => {
+  const classes = useStyles();
+  const data = props.data.innerBlocks[0];
+  const imageList = data.innerBlocks[0].innerBlocks;
+  const contentList = handleData(data.innerBlocks[1].innerBlocks)
+
+  return (
+    <div className={classes.root}>
+      <div className={`${classes.container} ${classes.sectionContent}`}>
+        <img
+          className={classes.imageLogos}
+          src={imageList[0].attributes.url}
+          alt={imageList[0].attributes.url}
+        />
+        <div className={classes.innerContent}>
+          {/* <h3>Start your stress-free PTE preparation today.</h3>
+          <p>
+            You are just one step closer to your dream. Master PTE with this powerful tool.
+          </p> */}
+          {contentList}
+          <Link href="/platform">
+            <Button className={classes.signUpButton}>Get Started</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SeventhSection;
