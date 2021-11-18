ea3983c1639d825d97a1b9363504a9db4a36dd92
test <huynhnhatkhoa21@gmail.com>
2021-11-18 15:06:02 +0700

    Complete Section 1

--- pages/blogs/blogs-section/Section1.jsx
+++ pages/blogs/blogs-section/Section1.jsx
@@ -27,10 +27,47 @@ const useStyles = makeStyles((theme) => ({
       padding: 0,
     },
   },
+  styleGrid:{
+    padding:"30px 100px 25px 100px",
+    marginBottom:25,
+    "& h1":{
+      fontSize:48,
+      "& a":{
+       width:"100%",
+      },
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding:0,
+      paddingTop:20,
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center !important",
+      textAlign:"center",
+    },
+  },
+  styleImage:{
+    width:"100%",
+    height:"100%"
+  },
+  styleText:{
+    width:"100% !important",
+    "& p":{
+       width:"100%",
+       fontSize:20,
+    },
+  },
+  // imageContainer:{
+  //   [theme.breakpoints.down("sm")]:{
+  //     width:"100%",
+  //   },
+  // },
 }));
 
 export default function Section1(props) {
   const classes = useStyles();
+  const data = props.data[0];
+  const image = data.featuredImage.node;
+  const categories = data.categories.nodes[0];
   const posts = props.data.map((post, index) => {
     if (index != 0) {
       let date = "";
@@ -42,24 +79,42 @@ export default function Section1(props) {
         }
       }
       return (
-        <Grid item xs={12} md={3} sm={6} className={classes.gridItem}>
-          <div className={classes.categories}>
-            <p>{post.categories.nodes.map((category) => category.name)}</p>
-          </div>
-
-          <Link href="#">
-            <h2>
-              <a>{post.title}</a>
-            </h2>
-          </Link>
-          <p>{date}</p>
-        </Grid>
+          <Grid item xs={12} md={3} sm={6} className={classes.gridItem} >
+            <div className={classes.categories}>
+              <p>{post.categories.nodes.map((category) => category.name)}</p>
+            </div>
+            <Link href="#">
+              <h2>
+                <a>{post.title}</a>
+              </h2>
+            </Link>
+            <p>{date}</p>
+          </Grid>
       );
     }
   });
 
   return (
     <div className={classes.root}>
+      <Grid container className={classes.styleGrid} justifyContent="center">
+        <Grid item  xs={12} sm={12} md={6} className={classes.styleGridText}>
+          <a href="/">{categories.name}</a>  
+          <h1>
+            <a href="/">{data.title}</a>
+          </h1>
+          <div
+          dangerouslySetInnerHTML={{
+            __html: data.excerpt,
+          }}
+          className={classes.styleText}
+        ></div>
+        </Grid>
+        <Grid item xs={12} md={6} sm={12} className={classes.imageContainer}>
+          <a href="/" >
+            <img src={image.sourceUrl} className={classes.styleImage}></img>
+          </a>
+        </Grid>
+      </Grid>
       <div>Lastest Post</div>
       <Grid container>{posts}</Grid>
     </div>
